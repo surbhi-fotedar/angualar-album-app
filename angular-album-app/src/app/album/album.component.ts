57eba69e91168ab6147f0de7c3ec078fdc85ca06
Surbhi Fotedar <surbhif@CESCHNDES-0108.CESCHN.COM>
2018-12-17 19:31:22 +0530

    search album and sorting albums by name is working

--- angular-album-app/src/app/album/album.component.ts
+++ angular-album-app/src/app/album/album.component.ts
@@ -11,6 +11,11 @@ import { IAlbum } from '../ialbum';
 export class AlbumComponent implements OnInit {
 
   public albumArray: IAlbum[];
+         srchAlbm: string;
+         disAlbm: string;
+         albmFound: boolean;
+         options = ['ascending', 'descending'];
+         optionSelected: any;
 
   constructor(private apiService: ApiService) { }
 
@@ -19,6 +24,36 @@ export class AlbumComponent implements OnInit {
         .subscribe( response => this.albumArray = response,
         error => console.log('Error ::' + error));
   }
+
+  displayAlbum() {
+    for(let i of this.albumArray) {
+      if(this.srchAlbm == i.title ) {
+        this.albmFound = true;
+        this.disAlbm = i.title;
+      }
+    }
+  }
+
+  onOptionsSelected(event){
+    
+    if(event == 'ascending') {
+
+      this.albumArray.sort((a, b) =>
+       
+      (a.title < b.title) ?  //sort string ascending
+       -1 :
+     ((a.title > b.title) ? 1 : 0
+       //default return value (no sorting)
+ ));
+    } else {
+      this.albumArray.sort((a,b) =>
+      (a.title > b.title) ? // sort in descending
+      -1:
+      ((a.title < b.title) ? 1 : 0
+    ));
+    }
+   
+   }
 
   ngOnInit(): void {
     this.getAlbums();
